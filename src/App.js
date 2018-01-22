91594aa9a35f06262d7e3cf3bf9ad10aba020c0d
AlinaKurochka <alinayaremachenko@gmail.com>
2018-01-22 20:20:31 +0200

    delited comments

--- src/App.js
+++ src/App.js
@@ -8,14 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        // <Navbar />
-        // < />
-        // < />
-        // < />
-        // < />
-        // < />
-        // < />
-        // < />
+      
       </div>
     );
   }
